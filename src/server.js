d99efb67968a9cb0b81b06e24d2518e9d028200a
Henrique1406 <phfn.nascimento.10@gmail.com>
2026-04-07 23:21:31 -0300

    Alteração user e pass do transporter. Ambos agora vem do .env

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-                user: 'hangardabaleia@gmail.com',
+                user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS}
 })
 
